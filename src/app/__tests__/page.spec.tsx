2ca9167f8025dd314bdaacf861acbf742ee81e2d
MattElcock <mattiose@outlook.com>
2024-01-11 20:22:13 +0000

    chore: jest

---
+++ src/app/__tests__/page.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import Home from "@/app/page";
+
+describe("page", () => {
+  it("should render hello world", () => {
+    const screen = render(<Home />);
+
+    expect(screen.getByText("Hello World")).toBeInTheDocument();
+  });
+});
