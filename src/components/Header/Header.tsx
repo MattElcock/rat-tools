bea1740179e55294ebf4b0b2b18bb78459301bd9
MattElcock <mattiose@outlook.com>
2024-01-11 21:20:51 +0000

    feat: header layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const Header = () => {
+  return (
+    <Box>
+      <Text fontSize="2xl" as="h1">
+        Rat Tools
+      </Text>
+    </Box>
+  );
+};
+
+export { Header };
