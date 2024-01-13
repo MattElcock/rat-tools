2cff215031cb6407a140ecb05219c19813e57909
Matt <matthew.elcock82@googlemail.com>
2024-01-13 21:59:07 +0000

    feat/footer (#7)
    * feat/footer
    
    * feat/footer
    
    ---------
    
    Co-authored-by: MattElcock <mattiose@outlook.com>

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,28 @@
+import { Box, Link, Stack, Text } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box>
+      <Stack fontSize="sm">
+        <Text>
+          If you are concerned about the health or wellbeing of your pet, please
+          consult a veterinary professional.
+        </Text>
+        <Text>
+          Curious rats image by{" "}
+          <Link
+            href="https://www.freepik.com/free-vector/hand-drawn-rat-silhouette_49273747.htm"
+            color="teal.500"
+            target="_blank"
+            rel="noopener"
+          >
+            Freepik
+          </Link>
+          .
+        </Text>
+      </Stack>
+    </Box>
+  );
+};
+
+export { Footer };
