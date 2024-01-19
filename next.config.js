e5e67d1fc0ee55c05ffee61e645c8df0127a6740
MattElcock <mattiose@outlook.com>
2024-01-19 17:17:28 +0000

    feat/connect-to-strapi

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.API_URL}/:path*`,
+        destination: `http://${process.env.API_URL}/:path*`,
         basePath: false,
       },
     ];
