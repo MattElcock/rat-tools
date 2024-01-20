dff49daba7bbe02db488bac88f303e44e57b6394
MattElcock <mattiose@outlook.com>
2024-01-20 22:35:17 +0000

    fix: unused import

--- src/app/pets/[petId]/page.tsx
+++ src/app/pets/[petId]/page.tsx
@@ -1,6 +1,5 @@
 import { PetProfile } from "@/features/PetProfile";
 import { PageProps } from "@/types";
-import { Stack } from "@chakra-ui/react";
 
 export default function PetProfilePage(props: PageProps) {
   const { petId } = props.params;
