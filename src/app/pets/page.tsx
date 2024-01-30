76a8d0dab9e72b50e83dfe5918a23dec4ffd661c
MattElcock <mattiose@outlook.com>
2024-01-30 23:00:23 +0000

    refactor: tidy up

--- src/app/pets/page.tsx
+++ src/app/pets/page.tsx
@@ -1,22 +1,5 @@
 import { PetMenu } from "@/features/PetMenu";
-import { gql } from "@urql/core";
 import { Suspense } from "react";
-
-const query = gql`
-  query {
-    groups {
-      pets {
-        id
-        name
-        latestWeight {
-          value
-          dateTaken
-          metric
-        }
-      }
-    }
-  }
-`;
 
 export default async function UserHomepage() {
   return (
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -14,9 +14,9 @@ import { useMemo } from "react";
 export function Providers({ children }: { children: React.ReactNode }) {
   const [client, ssr] = useMemo(() => {
     const ssr = ssrExchange();
-    const clientUrl = `${
-      process.env.NEXT_PUBLIC_IS_LOCAL ? "http" : "https"
-    }://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`;
+    const clientUrl = `${process.env.NEXT_PUBLIC_VERCEL ? "https" : "http"}://${
+      process.env.NEXT_PUBLIC_VERCEL_URL
+    }/api/graphql`;
     const client = createClient({
       url: clientUrl,
       exchanges: [cacheExchange, ssr, fetchExchange],
