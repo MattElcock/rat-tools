9b47a3b04b0a4bf05204c41c10514a22da058dd5
Matt <matthew.elcock82@googlemail.com>
2024-02-04 11:49:18 +0000

    Feat/weight tracker datatable (#25)
    * feat: weight tracker datatable
    
    * test: update tests
    
    * fix: types
    
    ---------
    
    Co-authored-by: MattElcock <mattiose@outlook.com>

--- src/api/getWeightsByPetId.ts
+++ src/api/getWeightsByPetId.ts
@@ -2,23 +2,21 @@ import { gql } from "@urql/core";
 import { useQuery } from "@urql/next";
 
 const query = gql`
-  query ($id: String!) {
-    getPetById(id: $id) {
-      weights {
-        dateTaken
-        value
-      }
+  query ($id: String!, $from: String, $to: String) {
+    getWeightsByPetId(petId: $id, from: $from, to: $to) {
+      value
+      dateTaken
     }
   }
 `;
 
-const useGetWeightsByPetId = (id: string) => {
-  const [result] = useQuery({ query, variables: { id } });
+const useGetWeightsByPetId = (id: string, from: string, to: string) => {
+  const [result] = useQuery({ query, variables: { id, from, to } });
 
   return {
     isLoading: result.fetching,
     error: result.error,
-    data: result.data?.getPetById,
+    data: result.data?.getWeightsByPetId,
   };
 };
 
---
+++ src/features/WeightTracker/components/DataTable.tsx
@@ -0,0 +1,114 @@
+import { useGetWeightsByPetId } from "@/api/getWeightsByPetId";
+import { DataPeriod, DateRange } from "@/components/PeriodFilter";
+import {
+  SkeletonText,
+  Table,
+  TableCellProps,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import moment from "moment";
+
+interface Props {
+  petId: string;
+  dateRange: DateRange;
+  currentPeriod: DataPeriod;
+}
+
+const RowHeading = ({ children, ...rest }: TableCellProps) => {
+  return (
+    <Td scope="row" {...rest}>
+      {children}
+    </Td>
+  );
+};
+
+const DataTable = ({ petId, dateRange, currentPeriod }: Props) => {
+  const { data, isLoading } = useGetWeightsByPetId(
+    petId,
+    dateRange.start,
+    dateRange.end
+  );
+
+  const dayFormat = currentPeriod === DataPeriod.Week ? "dddd" : "dddd, Do";
+
+  if (isLoading) {
+    return (
+      <TableContainer>
+        <Table variant="striped">
+          <Thead>
+            <Tr>
+              <Th textTransform="capitalize">Day</Th>
+              <Th textTransform="capitalize" isNumeric>
+                Weight
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <RowHeading>
+                <SkeletonText noOfLines={1} />
+              </RowHeading>
+              <Td isNumeric>
+                <SkeletonText noOfLines={1} />
+              </Td>
+            </Tr>
+            <Tr>
+              <RowHeading>
+                <SkeletonText noOfLines={1} />
+              </RowHeading>
+              <Td isNumeric>
+                <SkeletonText noOfLines={1} />
+              </Td>
+            </Tr>
+            <Tr>
+              <RowHeading>
+                <SkeletonText noOfLines={1} />
+              </RowHeading>
+              <Td isNumeric>
+                <SkeletonText noOfLines={1} />
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
+    );
+  }
+
+  return (
+    <TableContainer>
+      <Table variant="striped">
+        <Thead>
+          <Tr>
+            <Th textTransform="capitalize">Day</Th>
+            <Th textTransform="capitalize" isNumeric>
+              Weight
+            </Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data.length > 0 &&
+            data.map((weight: any) => (
+              <Tr key={weight.dateTaken}>
+                <RowHeading>
+                  {moment(weight.dateTaken).format(dayFormat)}
+                </RowHeading>
+                <Td isNumeric>{weight.value}g</Td>
+              </Tr>
+            ))}
+          {data.length === 0 && (
+            <Tr>
+              <Td colSpan={2}>No data</Td>
+            </Tr>
+          )}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export { DataTable };
