eb3167b204b95c2e4bb2daf67be21968d6360a79
MattElcock <mattiose@outlook.com>
2024-02-18 21:15:28 +0000

    refactor/breadcrumb-swap-out-uuid

--- src/features/Breadcrumbs/Breadcrumbs.tsx
+++ src/features/Breadcrumbs/Breadcrumbs.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import theme from "@/app/theme";
+import { useGetPetById } from "@/api/root/getPetById";
 import {
   Box,
   Breadcrumb,
@@ -8,14 +8,65 @@ import {
   BreadcrumbLink,
   Text,
 } from "@chakra-ui/react";
-import { upperFirst } from "lodash";
+import { startCase } from "lodash";
 import NextLink from "next/link";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { IoArrowForwardOutline } from "react-icons/io5";
+
+const buildLink = (path: string[], breadcrumbIndex: number) =>
+  `/${path.slice(0, breadcrumbIndex + 1).join("/")}`;
+
+const formatBreadcrumb = (text: string) => startCase(text.replace("-", " "));
+
+interface PetNameBreadcrumbProps {
+  link?: string;
+}
+
+const PetNameBreadcrumb = ({ link }: PetNameBreadcrumbProps) => {
+  const { petId } = useParams();
+  const { data } = useGetPetById(petId as string);
+
+  if (link) {
+    return (
+      <BreadcrumbLink href={link} as={NextLink}>
+        {formatBreadcrumb(data.name)}
+      </BreadcrumbLink>
+    );
+  }
+
+  return <>{formatBreadcrumb(data.name)}</>;
+};
 
 const Breadcrumbs = () => {
   const pathname = usePathname();
   const pathAsArray = pathname.split("/").filter(Boolean);
+
+  const buildBreadcrumbItem = (path: string[], breadcrumbIndex: number) => {
+    if (pathAsArray[breadcrumbIndex - 1] === "pets") {
+      return (
+        <PetNameBreadcrumb
+          link={
+            pathAsArray.length - 1 === breadcrumbIndex
+              ? undefined
+              : buildLink(pathAsArray, breadcrumbIndex)
+          }
+        />
+      );
+    }
+
+    if (pathAsArray.length - 1 === breadcrumbIndex) {
+      return <>{formatBreadcrumb(path[breadcrumbIndex])}</>;
+    }
+
+    return (
+      <BreadcrumbLink
+        href={buildLink(pathAsArray, breadcrumbIndex)}
+        as={NextLink}
+      >
+        {formatBreadcrumb(path[breadcrumbIndex])}
+      </BreadcrumbLink>
+    );
+  };
 
   return (
     <Box fontSize="sm" display="flex" gap={2}>
@@ -23,16 +74,11 @@ const Breadcrumbs = () => {
         You are here:
       </Text>
       <Breadcrumb spacing={1} separator={<IoArrowForwardOutline />}>
-        {pathAsArray.map((pathSegment, i) => {
-          const link = pathAsArray.filter((_, j) => j <= i).join("/");
-          return (
-            <BreadcrumbItem key={i}>
-              <BreadcrumbLink href={`/${link}`} as={NextLink}>
-                {upperFirst(pathSegment)}
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-          );
-        })}
+        {pathAsArray.map((item, i) => (
+          <BreadcrumbItem key={item}>
+            {buildBreadcrumbItem(pathAsArray, i)}
+          </BreadcrumbItem>
+        ))}
       </Breadcrumb>
     </Box>
   );
