48f89605bddf91eb6f52e51814fd06e19256888a
MattElcock <mattiose@outlook.com>
2024-02-18 21:43:28 +0000

    fix: caching

--- src/app/pets/page.tsx
+++ src/app/pets/page.tsx
@@ -1,25 +1,9 @@
-import { getClient } from "@/api/client";
 import { PetMenu } from "@/features/PetMenu";
-import { gql } from "@apollo/client";
 import { Suspense } from "react";
 
 export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
 
 export default async function UserHomepage() {
-  const query = gql(`
-    query {
-      groups {
-        pets {
-          id
-          name
-        }
-      }
-    }
-  `);
-
-  const { data } = await getClient().query({ query });
-
   return (
     <Suspense>
       <PetMenu />
